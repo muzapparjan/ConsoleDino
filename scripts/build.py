70c9f7cf7543515e3da5857b14415646368a7ff9
muzappar <muzappar.ce@foxmail.com>
2024-04-11 14:22:47 +0800

    feature: added clear_screen

--- scripts/build.py
+++ scripts/build.py
@@ -1,5 +1,14 @@
+import argparse
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+                    prog='ConsoleDino',
+                    description='The mini dinosaur escaped from Chrome to console!',
+                    epilog='--dino-dino-dino-dino-dino-dino--')
+    parser.add_argument("-c", "--compiler", type=str, default="gcc")
+    # TODO
+    args = parser.parse_args()
+    # TODO
 
 if __name__ == "__main__":
     main()
